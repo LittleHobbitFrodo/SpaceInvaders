bc6cf1ba4da086815ed56ff60864d4812812cac1
Samwise Gamgee <kiwi_sh@icloud.com>
2024-09-17 13:53:08 +0200

    repo init

---
+++ SpaceInvaders/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SaceInvaders {
+	class SpaceInvaders {
+		public static void Main(string[] args) {
+			Console.Title = "Space Invaders";
+			Console.WriteLine("Welcome to the SpaceInvaders game!");
+		}				
+	}
+}
